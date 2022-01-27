9b76fdc85263ff40636ab3be57f4d0fc38adfb80
Your Name <you@example.com>
2022-01-27 13:23:55 +0000

    test

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 
-#test receive_1
+#test1 receive_1
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50))
