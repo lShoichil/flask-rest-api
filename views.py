374d5644ae17043d8d1897d08fc3ba6d7c051696
Shoichi <leningdenis@gmail.com>
2022-05-05 21:00:12 +1000

    Update

--- views.py
+++ views.py
@@ -10,211 +10,211 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import app
 from models import *
 
-
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-        if "x-access-token" in request.headers:
-            token = request.headers["x-access-token"]
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
 
         if not token:
-            return jsonify({"message": "Token is missing!"}), 401
+            return jsonify({'message': "Token is missing!"}), 401
 
         try:
-            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
-            current_user = User.query.filter_by(public_id=data["public_id"]).first()
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+                              algorithms=["HS256"])
+            current_user = \
+                User.query.filter_by(public_id=data['public_id']).first()
+
         except:
-            return jsonify({"message": "Token is invalid!"}), 401
+            return jsonify({'message': "Token is invalid!"}), 401
 
         return f(current_user, *args, **kwargs)
 
     return decorated
 
 
-dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
-
-@app.route("/")
-def hello():
-    return "<h1>Hello There!</h1>"
 
 
 @app.route("/health/", methods=["GET"])
 def health():
-    return jsonify({"env": os.environ["SERVER_ENV"]})
+    return jsonify({
+        "env": os.environ['SERVER_ENV']
+    })
 
 
-@app.route("/login")
+@app.route('/')
+def hello():
+    return "<h1>Hello There!</h1>"
+
+
+@app.route('/user', methods=['GET'])
+@token_required
+def get_all_users(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot perform that function!'})
+
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {'public_id': user.public_id, 'name': user.name, 'password': user.password, 'admin': user.admin}
+        output.append(user_data)
+
+    return jsonify({'users': output})
+
+
+@app.route('/user/<public_id>', methods=['GET'])
+@token_required
+def get_one_user(current_user, public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    user_data = {'public_id': user.public_id, 'name': user.name, 'password': user.password, 'admin': user.admin}
+
+    return jsonify({'user': user_data})
+
+
+@app.route('/user', methods=['POST'])
+@token_required
+def create_user(current_user):
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'],
+                    password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'New user created!'})
+
+
+@app.route('/user/<public_id>', methods=['PUT'])
+@token_required
+def promote_user(current_user, public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    user.admin = True
+    db.session.commit()
+
+    return jsonify({'message': 'The user has been promoted!'})
+
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+@token_required
+def delete_user(current_user, public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'The user has been deleted!'})
+
+
+@app.route('/login')
 def login():
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
-        return make_response(
-            "Could not verify",
-            401,
-            {"WWW-Authenticate": "Basic realm=" '"Login required!"'},
-        )
+        return make_response('Could not verify', 401,
+                             {
+                                 'WWW-Authenticate': 'Basic realm='
+                                                     '"Login required!"'
+                             })
 
     user = User.query.filter_by(name=auth.username).first()
 
     if not user:
-        return make_response(
-            "Could not verify",
-            401,
-            {"WWW-Authenticate": "Basic realm=" '"Login required!"'},
-        )
+        return make_response('Could not verify', 401,
+                             {
+                                 'WWW-Authenticate': 'Basic realm='
+                                                     '"Login required!"'
+                             })
 
     if check_password_hash(user.password, auth.password):
         token = jwt.encode(
-            {
-                "public_id": user.public_id,
-                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            },
-            app.config["SECRET_KEY"],
-            algorithm="HS256",
-        )
-        return jsonify({"token": token})
+            {'public_id': user.public_id,
+             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)},
+            app.config['SECRET_KEY'], algorithm="HS256")
+        return jsonify({'token': token})
 
-    return make_response(
-        "Could not verify", 401, {"WWW-Authenticate": 'Basic realm="Login required!"'}
-    )
+    return make_response('Could not verify', 401,
+                         {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
 
-@app.route("/user", methods=["GET", "POST"])
-@app.route("/user/<public_id>", methods=["GET", "PUT", "DELETE"])
+@app.route('/todo', methods=['GET'])
 @token_required
-def work_to_user(public_id=None, current_user=None):
-    # Вывод всех пользователей, если вы администратор
-    if request.method == "GET" and public_id is None:
-        if not current_user.admin:
-            return jsonify({"message": "Cannot perform that function!"})
+def get_all_todos(current_user):
+    todos = Todo.query.filter_by(user_id=current_user.id).all()
 
-        users = User.query.all()
+    output = []
 
-        output = []
+    for todo in todos:
+        todo_data = {'id': todo.id, 'text': todo.text, 'complete': todo.complete}
+        output.append(todo_data)
 
-        for user in users:
-            user_data = {
-                "public_id": user.public_id,
-                "name": user.name,
-                "password": user.password,
-                "admin": user.admin,
-            }
-            output.append(user_data)
-
-        return jsonify({"users": output})
-
-    # Создание пользователя
-    if request.method == "POST":
-        data = request.get_json()
-
-        hashed_password = generate_password_hash(data["password"], method="sha256")
-
-        new_user = User(
-            public_id=str(uuid.uuid4()),
-            name=data["name"],
-            password=hashed_password,
-            admin=False,
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({"message": "New user created!"})
-
-    # Получение одного пользователя
-    if request.method == "GET" and public_id is not None:
-        user = User.query.filter_by(public_id=public_id).first()
-
-        if not user:
-            return jsonify({"message": "No user found!"})
-
-        user_data = {
-            "public_id": user.public_id,
-            "name": user.name,
-            "password": user.password,
-            "admin": user.admin,
-        }
-
-        return jsonify({"user": user_data})
-
-    # Дать права администратора
-    if request.method == "PUT":
-        user = User.query.filter_by(public_id=public_id).first()
-
-        if not user:
-            return jsonify({"message": "No user found!"})
-
-        user.admin = True
-        db.session.commit()
-
-        return jsonify({"message": "The user has been promoted!"})
-
-    # Удаление пользователя
-    if request.method == "DELETE":
-        user = User.query.filter_by(public_id=public_id).first()
-
-        if not user:
-            return jsonify({"message": "No user found!"})
-        db.session.delete(user)
-        db.session.commit()
-
-        return jsonify({"message": "The user has been deleted!"})
+    return jsonify({'todos': output})
 
 
-@app.route("/todo", methods=["GET", "POST"])
-@app.route("/todo/<todo_id>", methods=["GET", "PUT", "DELETE"])
+@app.route('/todo/<todo_id>', methods=['GET'])
 @token_required
-def todo_list(current_user=None, todo_id=None):
-    # Вывод всех задач
-    if request.method == "GET" and todo_id is None:
-        todos = Todo.query.filter_by(user_id=current_user.id).all()
+def get_one_todo(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
 
-        output = []
+    if not todo:
+        return jsonify({'message': 'No todo found'})
 
-        for todo in todos:
-            todo_data = {"id": todo.id, "text": todo.text, "complete": todo.complete}
-            output.append(todo_data)
+    todo_data = {'id': todo.id, 'text': todo.text, 'complete': todo.complete}
 
-        return jsonify({"todos": output})
-    # Вывод одной задачи
-    if request.method == "GET" and todo_id is not None:
-        todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+    return jsonify(todo_data)
 
-        if not todo:
-            return jsonify({"message": "No todo found"})
 
-        todo_data = {"id": todo.id, "text": todo.text, "complete": todo.complete}
+@app.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+    data = request.form['text']
 
-        return jsonify(todo_data)
-    # Создание задачи
-    if request.method == "POST":
-        data = request.form["text"]
+    new_todo = Todo(text=data, complete=False, user_id=current_user.id)
+    db.session.add(new_todo)
+    db.session.commit()
 
-        new_todo = Todo(text=data, complete=False, user_id=current_user.id)
-        db.session.add(new_todo)
-        db.session.commit()
+    return jsonify({'message': 'Todo created!'})
 
-        return jsonify({"message": "Todo created!"})
-    # Изменить состояние задачи
-    if request.method == "PUT":
-        todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
 
-        if not todo:
-            return jsonify({"message": "No todo found"})
+@app.route('/todo/<todo_id>', methods=['PUT'])
+@token_required
+def change_todo(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
 
-        todo.complete = True
-        db.session.commit()
+    if not todo:
+        return jsonify({'message': 'No todo found'})
 
-        return jsonify({"message": "Todo item has been completed!"})
-    # Удаление задачи
-    if request.method == "DELETE":
-        todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+    todo.complete = True
+    db.session.commit()
 
-        if not todo:
-            return jsonify({"message": "No todo found"})
+    return jsonify({'message': 'Todo item has been completed!'})
 
-        db.session.delete(todo)
-        db.session.commit()
 
-        return jsonify({"message": "Todo item deleted!"})
+@app.route('/todo/<todo_id>', methods=['DELETE'])
+@token_required
+def delete_todo(current_user, todo_id):
+    todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+    if not todo:
+        return jsonify({'message': 'No todo found'})
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return jsonify({'message': 'Todo item deleted!'})
+
