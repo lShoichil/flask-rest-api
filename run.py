ea4e0ebc9a5cf6f985458b8de8baa99cb883666b
Shoichi <leningdenis@gmail.com>
2022-05-08 23:26:06 +1000

    small update

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(debug=True, port=5000, host="0.0.0.0")
+        app.run(debug=True, port=5001, host="0.0.0.0")
